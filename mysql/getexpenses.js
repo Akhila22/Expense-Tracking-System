2eddd8c76029f0c3d4fbe2df18c2634434e3144a
cscsandy5 <cscsandy5@gmail.com>
2017-08-26 18:15:52 +0530

    removed log statements

--- mysql/getexpenses.js
+++ mysql/getexpenses.js
@@ -1,10 +1,8 @@
 exports.getExpenseTbl = function(req, res){
   trycatch(function(){
-    console.log(req.session.user_id);
     if(!req.session.user_id){
       throw new Error("User not logged in");
     }
-    console.log('getexpense back end file');
     var sql = "select expense_id,amount,e.date,description,category_name," +
               "vendor_name FROM expenses as e, vendor v,category c WHERE " +
               "e.category_id = c.category_id and e.vendor_id = v.vendor_id " +
