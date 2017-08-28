20f6e73b4f2cbefcf701411603dfeeb168d98161
Sivani <sivani.karnatakam@gmail.com>
2017-08-28 14:53:17 +0530

    expense owerflow mail
    expense owerflow mail

--- mysql/mailernotify.js
+++ mysql/mailernotify.js
@@ -1,9 +1,13 @@
 var uuid =require('uuid');
-exports.sendmailnotify = function(req,res){
+exports.sendmailnotify = function(req,res,quarter_number,quarter_budget,user_email,admin_email,category_name,financial_year){
 
     trycatch(function(){
+    	
+        	
 server.start();
 // create reusable transporter object using SMTP transport
+
+
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
@@ -11,7 +15,11 @@ server.start();
             pass: 'maheshbabu1'
         }
     });
-var maillist='boddupalliaditya@gmail.com';
+    var email_id_pat = user_email+","+admin_email;
+    console.log(user_email);
+    console.log(email_id_pat);
+    
+var maillist=email_id_pat;
 // setup e-mail data with unicode symbols
     var mailOptions = {
 // sender address
@@ -23,8 +31,8 @@ var maillist='boddupalliaditya@gmail.com';
 // plaintext body
         text: 'It works ?',
 // rich text html body
-        html: "<p>This is the dummy passowrd for your authentication. Please change your password when you login with dummy password</p>"+
-        "<b>Dummy passowrd:</b> ",
+        html: "<p>Budget for quarter Q"+quarter_number+" has exceeded for category "+cat_name+". Remaining quarter budget = "+quarter_budget+" for financial year "+financial_year+"</p>",
+        
     };
 transporter.sendMail(mailOptions, function(error, info){
     if(error){
