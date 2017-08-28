c93d81bede8559514a82f74d53fdf4e393133681
Aditya-Boddupalli <31129705+Aditya-Boddupalli@users.noreply.github.com>
2017-08-28 12:09:20 +0530

    Notification mails

--- mysql/addexpense.js
+++ mysql/addexpense.js
@@ -38,8 +38,8 @@ exports.addExp = function(req, res) {
     var sqlBudget = "select remaining_budget from category_financial_year where category_id="+category_id +" AND financial_year ='"+financial_year+"'"; 
     connection.query(sqlBudget,function(error,rows,fields){
     	if(!!error){
-    		console.log("error in query");
-             throw new Error("Error in the query");
+    		console.log("error in query 1");
+             throw new Error("Error in the query 1");
         }
         else{
             remaining_budget = parseInt(rows[0].remaining_budget) - amount;
@@ -51,8 +51,8 @@ exports.addExp = function(req, res) {
      connection.query(sqlQuarterBudget,function(error,rows,fields){
     	 if(!!error){
     		 console.log(sqlQuarterBudget);
-    		 console.log("error in query");
-              throw new Error("Error in the query");
+    		 console.log("error in query 2");
+              throw new Error("Error in the query 2");
        	 }
        	 else{
              quarter_budget = parseInt(rows[0].quarter_budget) - amount;
@@ -63,57 +63,50 @@ exports.addExp = function(req, res) {
      var sql2 = "select vendor_id from vendor where vendor_name = '"+vendorname+"';";
      connection.query(sql2, function(error,rows,fields){
     	 if(!!error){
-    		 console.log('Error in the query');
-              throw new Error("Error in the query");
+    		 console.log('Error in the query 3');
+              throw new Error("Error in the query 3");
          }
          else{
         	 if(typeof rows[0] == "undefined"){
         		 var sql3="insert into vendor(vendor_name) values('"+vendorname+"');";
                  connection.query(sql3, function(error,rows,fields){
                 	 if(!!error){
-                		 console.log('Error in the query');
-                          throw new Error("Error in the query");
+                		 console.log('Error in the query 4');
+                          throw new Error("Error in the query 4");
                      }
                      else{
                     	 var sql4 = "select vendor_id from vendor where vendor_name = '"+vendorname+"';";
                          connection.query(sql4, function(error,rows1,fields){
                         	 if(!!error){
-                        		 console.log('Error in the query');
-                                  throw new Error("Error in the query");
+                        		 console.log('Error in the query 5');
+                                  throw new Error("Error in the query 5");
                              }
                              else{
                             	 vid=rows1[0].vendor_id;
                                  var sql5 = "insert into expenses(user_id,category_id,amount,date,description,vendor_id,financial_year) values("+uid+","+category_id+","+amount+",'"+date+"','"+description+"',"+vid+",'"+financial_year+"')";
                                  connection.query(sql5, function(error,rows,fields){
                                 	 if(!!error){
-                                		 console.log('Error in the query');
-                                          throw new Error("Error in the query");
+                                		 console.log('Error in the query 6');
+                                          throw new Error("Error in the query 6");
                                      }
                                      else
                                      {
                                     	 console.log('Inserted successfully!!');
-                                    	 /*var quarter_wise = "UPDATE quarterwise_budget SET quarter_budget ="+ quarter_budget +" WHERE category_id = "+category_id+" AND financial_year = '"+financial_year+"' AND quarter_number="+quarter_number;
-                                    	 connection.query(quarter_wise,function(error,rows,fields){
-                                    		 console.log(sqlUpdate1);
-                                    		 if(!!error){
-                                    			 console.log("error in query");
-                                                  throw new Error("Error in the query");
-                                    		 }
-                                    		 else{
-                                    			 console.log('Updated');
-                                    		 }
-                                      	 });*/
+                                    	
                                          var sqlUpdate = "UPDATE category_financial_year SET remaining_budget ="+ remaining_budget +" WHERE category_id = "+category_id+" AND financial_year = '"+financial_year+"'";
                                          connection.query(sqlUpdate,function(error,rows,fields){
                                         	 console.log(sqlUpdate);
                                         	 if(!!error){
-                                        		 console.log("error in query");
-                                                  throw new Error("Error in the query");
+                                        		 console.log("error in query 7");
+                                                  throw new Error("Error in the query 7");
                                         	 }
                                         	 else{
                                         		 console.log('Updated');
                                         		 if(quarter_budget < 0){
                                         	    	 //mail logic
+                                               mailernotify.sendmailnotify(req,res);
+                                                      
+
                                         	     }
                                                   res.send(true);
                                         	 }
@@ -135,17 +128,6 @@ exports.addExp = function(req, res) {
                         }
                         else{
                         	console.log('Inserted successfully!!');
-                            /*var quarter_wise1 = "UPDATE quarterwise_budget SET quarter_budget ="+ quarter_budget +" WHERE category_id = "+category_id+" AND financial_year = '"+financial_year+"' AND quarter_number="+quarter_number;
-                            connection.query(quarter_wise1,function(error,rows,fields){
-                            	console.log(sqlUpdate1);
-                                if(!!error){
-                                	console.log("error in query");
-                                     throw new Error("Error in the query");
-                               	}
-                                else{
-                                	console.log('Updated');
-                               	}
-                             });*/
                              var sqlUpdate1 = "UPDATE category_financial_year SET remaining_budget ="+ remaining_budget +" WHERE category_id = "+category_id+" AND financial_year = '"+financial_year+"'";
                              connection.query(sqlUpdate1,function(error,rows,fields){
                             	 console.log(sqlUpdate1);
@@ -157,6 +139,8 @@ exports.addExp = function(req, res) {
                                       console.log('Updated');
                                       if(quarter_budget < 0){
                              	    	 //mail logic
+                                      mailernotify.sendmailnotify(req,res);
+
                              	     }
                                        res.send(true);
                                  }
---
+++ mysql/mailernotify.js
@@ -0,0 +1,40 @@
+var uuid =require('uuid');
+exports.sendmailnotify = function(req,res){
+
+    trycatch(function(){
+server.start();
+// create reusable transporter object using SMTP transport
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'boddupalliaditya@gmail.com',
+            pass: 'maheshbabu1'
+        }
+    });
+var maillist='boddupalliaditya@gmail.com';
+// setup e-mail data with unicode symbols
+    var mailOptions = {
+// sender address
+        from: 'boddupalliaditya@gmail.com', 
+// list of receivers
+        to: maillist, 
+// Subject line
+        subject: 'Mail from CA Education Team', 
+// plaintext body
+        text: 'It works ?',
+// rich text html body
+        html: "<p>This is the dummy passowrd for your authentication. Please change your password when you login with dummy password</p>"+
+        "<b>Dummy passowrd:</b> ",
+    };
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});
+},function(err){
+        console.log(err.stack);
+        res.send(false);
+    });
+};
