c12f2a519f532bd03f5559e05c3dfd031373e735
mereddyHarshitha <mereddy.harshitha@gmail.com>
2017-08-28 17:46:25 +0530

    category validation

--- mysql/addcategory.js
+++ mysql/addcategory.js
@@ -22,30 +22,54 @@ exports.addcategory= function(req, res) {
                 var Q4 = json.q4;
                 var user_id = req.session.user_id;
                 var date = json.date;
+                var cat_id;
                             //  console.log(req.body);
+
+              var str="select category_id from category where category_name='"+category_name+"'";
+              connection.query(str,function(error,rows,fields){
+                 if(!!error){
+                                          console.log('Error in the query');
+                                           throw new Error("Error in the query");
+                                           
+                                             }else
+                                             {
+                  cat_id=rows[0].category_id;
+                  console.log(cat_id);
+                 //// console.log("hello9");
+
+                   if(typeof cat_id=="undefined"){
+
+                console.log("hello");
+                console.log(cat_id);
+
 
            var sql1 = "insert into category(category_name,user_id,date) values('"+ category_name +"',"+ user_id +",'"+ date +"')";
                                           //   console.log(sql1);
                               connection.query(sql1, function(error,rows,fields){
                                              if(!!error){
                                           console.log('Error in the query');
-                                           throw new Error("Error in the query");
+                                           throw new Error("Error in the query"+error);
+                                           
                                              }else
                                              {
                         var sql ="select category_id from category where category_name='"+category_name+"'";
                                       connection.query(sql,function(err,result,fields){
                                                                if(err) throw err;
                                                      else{
-                              id =  result[0].category_id;
+                              cat_id =  result[0].category_id;
                                                   
                                      // console.log(id);
                           console.log('Category Inserted Successfully');
-                          
-               var sql2 = "insert into category_financial_year(category_id,financial_year,initial_budget,remaining_budget) values("+ id +",'"+ financial_year +"',"+initial_budget+","+initial_budget+")";
+                        }
+                        });
+                                    }
+                                  });
+            }
+            var sql2 = "insert into category_financial_year(category_id,financial_year,initial_budget,remaining_budget) values("+ cat_id +",'"+ financial_year +"',"+initial_budget+","+initial_budget+")";
                          // console.log(sql2);
                               connection.query(sql2, function(error,rows,fields){
                                              if(!!error){
-                                                            console.log('Error in the query');
+                                                            console.log('Error in the query2'+error);
                                                              throw new Error("Error in the query");
                                              }else{
 
@@ -56,7 +80,7 @@ exports.addcategory= function(req, res) {
                                                      [financial_year,id,3,Q3],
                                                      [financial_year,id,4,Q4]
                                                      ];
-                                    var sql3 = "insert into quarterwise_budget(financial_year,category_id,quarter_number,quarter_budget) VALUES ?";
+                                                var sql3 = "insert into quarterwise_budget(financial_year,category_id,quarter_number,quarter_budget) VALUES ?";
                                                connection.query(sql3,[values], function(error,result){
                                              if(!!error){
                                                             console.log('Error in the query');
@@ -69,12 +93,18 @@ exports.addcategory= function(req, res) {
     
                                              }
                               });
+                            }
+
+              });
+
+                          
+               
     
-                                             }
-                              });
+                                             
+                              
     
-                                                                                          }
-                                                                           });
+                                                                                          
+                                                                          
    // console.log(user_id);
    // res.send(user_id);
     },function(err){
